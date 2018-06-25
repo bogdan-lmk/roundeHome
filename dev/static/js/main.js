d961b3c1b47ebd45d23a6cc7eb5b6783fd9c7803
Bogdan Lameko <bogdan.lmk@gmail.com>
2018-06-25 18:09:03 +0300

    new

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,4 +2,23 @@ $(document).ready(function () {
     svg4everybody({});
 });
 
- 
+
+$('.btn-call').on('click', function(){
+  $('.contact-form--recall').addClass('contact-form-popup--active')
+  $('body').addClass('overflow-hidden')
+});
+
+$('.btn-help').on('click', function(){
+  $('.contact-form--help-area').addClass('contact-form-popup--active')
+  $('body').addClass('overflow-hidden')
+});
+
+$('.btn-project').on('click', function(){
+  $('.contact-form--help-build').addClass('contact-form-popup--active')
+  $('body').addClass('overflow-hidden')
+});
+
+$('.icon-close-icon').on('click', function(){
+  $('.contact-form-popup').removeClass('contact-form-popup--active');
+    $('body').removeClass('overflow-hidden')
+});
