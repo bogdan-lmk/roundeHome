2469f28d0f6b88d48aa701ff2960c3acd64eee3c
Bogdan Lameko <bogdan.lmk@gmail.com>
2018-06-26 14:49:13 +0300

    new

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -22,3 +22,38 @@ $('.icon-close-icon').on('click', function(){
   $('.contact-form-popup').removeClass('contact-form-popup--active');
     $('body').removeClass('overflow-hidden')
 });
+
+
+$(".header-nav-link--new").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".ultramodern-houses").offset().top},
+        'slow');
+});
+
+$(".header-nav-link--houses").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".circle-house").offset().top},
+        'slow');
+});
+
+$(".header-nav-link--types").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".houses-types").offset().top},
+        'slow');
+});
+
+$(".header-nav-link--contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contacts").offset().top},
+        'slow');
+});
+
+// 55°41'15.1"N 37°36'54.5"E
+// 55.687514, 37.615142
+function initMap() {
+  var center = {lat: 55.687514, lng: 37.615142 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: center
+  });
+}
